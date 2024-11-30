048f47e3a12f8703b2e2e6af75bf1f0a9dcbd66b
Doug Anderson444 <douganderson444@gmail.com>
2024-11-30 12:38:01 -0400

    switch back to allow static mut refs
    as dirs is breaking build

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(target_os = "linux", allow(static_mut_refs))] // dirs crate has warnings that break the CI build.
+#![allow(static_mut_refs)] // dirs crate has warnings that break the CI build.
 
 mod app;
 pub use app::MultinodeApp;
